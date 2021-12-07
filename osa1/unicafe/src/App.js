f4c8748c13eb7291b0983d28f9b1882d33e066e1
Valla Hallama <valla.hallama@gmail.com>
2021-12-07 12:47:38 +0200

    simplified

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -17,18 +17,21 @@ const Statistics = ({ good, neutral, bad }) => {
 			<div>
 				<h2>statistics</h2>
 				<table>
-				<StatisticLine text="good" value={good} />
-				<StatisticLine text="neutral" value={neutral} />
-				<StatisticLine text="bad" value={bad} />
-				<StatisticLine text="all" value={good + neutral + bad} />
-				<StatisticLine text="average" value={(good - bad) / (good + neutral + bad)} />
-				<StatisticLine text="positive" value ={good / (good + neutral + bad) * 100 + ' %'} />
+					<StatisticLine text="good" value={good} />
+					<StatisticLine text="neutral" value={neutral} />
+					<StatisticLine text="bad" value={bad} />
+					<StatisticLine text="all" value={good + neutral + bad} />
+					<StatisticLine text="average" value={(good - bad) / (good + neutral + bad)} />
+					<StatisticLine text="positive" value ={good / (good + neutral + bad) * 100 + ' %'} />
 				</table>
 			</div>
 		)
 	else
 		return (
-			<div><h2>statistics</h2>No feedback given</div>
+			<div>
+				<h2>statistics</h2>
+				No feedback given
+			</div>
 		)
 }
 
