4cc97f718a86a4fbe894f35f911bd8b88849a711
Valla Hallama <valla.hallama@gmail.com>
2021-12-10 13:53:59 +0200

    puhelinluettelo step6

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const Persons = ({ persons, newFilter }) => {
 	const ret = persons.filter(obj => obj.name.toLowerCase().search(newFilter.toLowerCase()) !== -1)
@@ -35,15 +36,21 @@ const Filter = ({ newFilter, handleFilterChange }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+	const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
 	const [newNumber, setNewNumber] = useState('')
 	const [newFilter, setNewFilter] = useState('')
+
+	useEffect(() => {
+		console.log('effect')
+		axios
+			.get('http://localhost:3001/persons')
+			.then(response => {
+				console.log('promise fulfilled')
+				setPersons(response.data)
+			})
+	}, [])
+	console.log('render', persons.length, 'persons')
 
 	const addContact = (event) => {
 		event.preventDefault()
