9b32786350e4b7abd1df0ea35f1263278abe7945
Valla Hallama <valla.hallama@gmail.com>
2021-12-15 13:53:06 +0200

    3a done

--- osa3/puhelinluettelo-backend/index.js
+++ osa3/puhelinluettelo-backend/index.js
@@ -4,7 +4,13 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan('tiny', {
+	skip: (req) => req.method === "POST"
+}))
+morgan.token('data', (req) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data', {
+	skip: (req) => req.method !== "POST"
+}))
 
 let persons = [
 	{
