2cfe93c9b0b9bc886d42bc75bbafc73b10cdc7d2
Valla Hallama <valla.hallama@gmail.com>
2021-12-16 12:20:00 +0200

    osa3b changes

--- osa3/puhelinluettelo-backend/index.js
+++ osa3/puhelinluettelo-backend/index.js
@@ -1,8 +1,10 @@
 const { response } = require('express')
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny', {
 	skip: (req) => req.method === "POST"
@@ -90,6 +92,6 @@ app.post('/api/persons', (req, res) => {
 	res.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
