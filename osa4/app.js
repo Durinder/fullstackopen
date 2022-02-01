1cbb164a21b6d989abdfb70c82217a2f0b11aefd
Valla Hallama <valla.hallama@gmail.com>
2022-02-01 17:34:08 +0200

    refactored blogsRouter

--- osa4/app.js
+++ osa4/app.js
@@ -1,31 +1,22 @@
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
 const mongoose = require('mongoose')
-const config = require('./utils/config')
-const Blog = require('./models/blog')
   
-  mongoose.connect(config.MONGODB_URI)
-  
-  app.use(cors())
-  app.use(express.json())
-  
-  app.get('/api/blogs', (request, response) => {
-	Blog
-	  .find({})
-	  .then(blogs => {
-		response.json(blogs)
-	  })
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
   })
-  
-  app.post('/api/blogs', (request, response) => {
-	const blog = new Blog(request.body)
-  
-	blog
-	  .save()
-	  .then(result => {
-		response.status(201).json(result)
-	  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
   })
 
-  module.exports = app
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
