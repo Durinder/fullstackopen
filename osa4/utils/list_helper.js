3348f926b9d3598f83f616de191e2885b27b4ad5
Valla Hallama <valla.hallama@gmail.com>
2022-02-03 15:06:44 +0200

    4.5 add favoriteBlog

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -1,4 +1,4 @@
-const dummy = (blogs) => {
+const dummy = () => {
 	return 1
 }
 
@@ -6,7 +6,18 @@ const totalLikes = (blogs) => {
 	return blogs.reduce((prev, cur) => prev + cur.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+	const favorite = blogs.reduce((prev, cur) => prev.likes > cur.likes ? prev : cur)
+
+	return {
+		title: favorite.title,
+		author: favorite.author,
+		likes: favorite.likes
+	}
+}
+
 module.exports = {
 	dummy,
-	totalLikes
+	totalLikes,
+	favoriteBlog
 }
