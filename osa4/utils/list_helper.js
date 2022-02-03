22c1d00616c5bb5327f6052f40e2da205f347249
Valla Hallama <valla.hallama@gmail.com>
2022-02-03 14:28:15 +0200

    4.4 total likes

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -1,7 +1,12 @@
 const dummy = (blogs) => {
 	return 1
 }
-  
+
+const totalLikes = (blogs) => {
+	return blogs.reduce((prev, cur) => prev + cur.likes, 0)
+}
+
 module.exports = {
-	dummy
+	dummy,
+	totalLikes
 }
