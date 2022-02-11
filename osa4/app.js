ae84f9f4760786ecdd6c8bfab7aa22222de1956e
Valla Hallama <valla.hallama@gmail.com>
2022-02-11 17:50:58 +0200

    add middleware

--- osa4/app.js
+++ osa4/app.js
@@ -7,6 +7,7 @@ const blogsRouter = require("./controllers/blogs")
 const logger = require("./utils/logger")
 const mongoose = require("mongoose")
 const usersRouter = require("./controllers/users")
+const middleware = require("./utils/middleware")
   
 mongoose.connect(config.MONGODB_URI)
 	.then(() => {
@@ -19,7 +20,12 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 
+app.use(middleware.requestLogger)
+
 app.use("/api/blogs", blogsRouter)
 app.use("/api/users", usersRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
