02076a5d050c8a6cdeece9ba9116ea8bf41dbf82
Valla Hallama <valla.hallama@gmail.com>
2022-03-23 17:12:10 +0200

    6.13-6.14 backend as json-server

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -2,8 +2,24 @@ import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import anecdoteService from './services/anecdotes'
+import { setAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const fetchAnecdotes = async () => {
+      const anecdotes = await anecdoteService.getAll()
+      dispatch(setAnecdotes(anecdotes))
+    }
+
+    fetchAnecdotes()
+      .catch(console.error)
+  }, [dispatch])
+
   return (
     <div>
       <h2>Anecdotes</h2>
